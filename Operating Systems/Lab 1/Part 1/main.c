7afc43e907f4b5f9948d6426f6c41dfe061f2609
Zhijia Chen <zachary_chen@hotmail.com>
2018-09-03 02:17:16 -0400

    .

--- Operating Systems/Lab 1/Part 1/main.c
+++ Operating Systems/Lab 1/Part 1/main.c
@@ -7,14 +7,18 @@
 #include "matrix.h"
 
 const unsigned int N = 1000;
+const unsigned int M = 10000;
+
+void insert_sort(int array[], unsigned int size, int new);
+
 int main(int argc, char const *argv[])
 {
+    //part 1
     double A[N][N];
     double B[N][N];
     double C[N][N];
     char order[6][3] = {{'i', 'j', 'k'}, {'i', 'k', 'j'}, {'j', 'i', 'k'}, {'j', 'k', 'i'}, {'k', 'i', 'j'}, {'k', 'j', 'i'}};
     void (*funArray[6])(double A[N][N], double B[N][N], double C[N][N]) = {matrix_multiplication0, matrix_multiplication1, matrix_multiplication2, matrix_multiplication3, matrix_multiplication4, matrix_multiplication5};
-    
     clock_t start, end;
     unsigned long cpu_time_used;
     srand(time(0));
@@ -36,6 +40,30 @@ int main(int argc, char const *argv[])
         cpu_time_used = (end - start) * 1000 / CLOCKS_PER_SEC;
         fprintf(fp, "%d %s,%s,%s %d\n", N, order[i][0], order[i][1], order[i][2], cpu_time_used);
     }
+
+    //part 2
+    int unsorted[M] ={0};
+    int sorted1[M], sorted2[M];
+    for (unsigned int i = 0; i < M; ++i)
+        unsorted[i] = rand();
     return 0;
+}
+
+void insert_sort(int array[], unsigned int size, int new)
+{
+    for (unsigned int i = size; i > 0; --i)
+    {
+        if (new < array[i-1])
+        {
+            array[i] = array[i-1];
+        }
+        else
+        {
+            array[i] = new;
+            break;
+        }
+    }
+    if (new < array[0])
+        array[0] = new;
 }
 
