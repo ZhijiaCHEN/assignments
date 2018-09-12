606404fcd3c4219b4d3686267b985c3fb5825143
Zhijia Chen <zachary_chen@hotmail.com>
2018-09-11 23:48:07 -0400

    .

---
+++ Operating Systems/Lab 1/Part 1/main.c
@@ -0,0 +1,78 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <assert.h>
+#include <string.h>
+#include "main.h"
+#include "matrix.h"
+
+const unsigned int N = 1000;
+
+int main(int argc, char const *argv[])
+{
+    int check = 0;
+    double (*A)[N][N] = calloc(N*N, sizeof(double));
+    double (*B)[N][N] = calloc(N*N, sizeof(double));
+    double (*C)[N][N] = calloc(N*N, sizeof(double));
+    char order[6][3] = {{'i', 'j', 'k'}, {'i', 'k', 'j'}, {'j', 'i', 'k'}, {'j', 'k', 'i'}, {'k', 'i', 'j'}, {'k', 'j', 'i'}};
+    void (*funArray[6])(double [N][N], double [N][N], double [N][N]) = {matrix_multiplication0, matrix_multiplication1, matrix_multiplication2, matrix_multiplication3, matrix_multiplication4, matrix_multiplication5};
+    clock_t start, end;
+    double cpu_time_used;
+    srand(time(0));
+    for (int r = 0; r < N; r++)
+    {
+        for (int c = 0; c < N; ++c)
+        {
+            (*A)[r][c] = (double)rand();
+            (*B)[r][c] = (double)rand();
+        }
+    }
+
+    if (check == 1)//check result
+    {
+        double (*D)[N][N] = calloc(N*N, sizeof(double));
+        funArray[0](*A, *B, *D);
+            for (int i = 1; i < 6; ++i)
+        {
+            funArray[i](*A, *B, *C);
+            for(size_t r = 0; r < N; ++r)
+            {
+                for(size_t c = 0; c < N; ++c)
+                    assert((*C)[r][c] == (*D)[r][c]);
+            }
+        }
+        free(D);
+    }
+    else
+    {
+        FILE *fp = fopen("matrix.log", "a");
+        for (int i = 0; i < 6; ++i)
+        {
+            start = clock();
+            funArray[i](*A, *B, *C);
+            end = clock();
+            cpu_time_used = (double)((end - start) * 1000) / CLOCKS_PER_SEC;
+            fprintf(fp, "%d %c,%c,%c %f\n", N, order[i][0], order[i][1], order[i][2], cpu_time_used);
+            memset(*C, 0, sizeof(double [N][N]));
+        }
+        fclose(fp);
+    }
+
+    free(A);
+    free(B);
+    free(C);
+    return 0;
+}
+
+void print_matrix(double X[N][N])
+{
+    for (size_t r = 0; r < N; ++r)
+    {
+        for (size_t c = 0; c < N; ++c)
+        {
+            printf("%f ", X[r][c]);
+        }
+        printf("\n");
+    }
+    printf("\n");
+}
---
+++ Operating Systems/Lab 1/Part 2/main.c
@@ -0,0 +1,111 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <assert.h>
+#include <string.h>
+
+const unsigned int N = 100;
+
+void insert_sort(int array[], size_t size);
+
+int main(int argc, char const *argv[])
+{
+    int repeat = 3;
+    int check = 0;
+
+    FILE *sortLog = fopen("sort.log", "w");
+    unsigned long insertSortTime = 0, mergeSortTime = 0;
+    clock_t start, end;
+    for (int r = 0; r < repeat; ++r)
+    {
+        int (*unsorted)[N] = calloc(sizeof(int), N);
+        int (*unsortedHalf1)[N / 2] = calloc(sizeof(int), N / 2);
+        int (*unsortedHalf2)[N - N / 2] = calloc(sizeof(int), N - N / 2);
+        for (unsigned int i = 0; i < N; ++i)
+            (*unsorted)[i] = rand();
+        memcpy(*unsortedHalf1, *unsorted, (N / 2)*sizeof(int));
+        memcpy(*unsortedHalf2, &((*unsorted)[N / 2]), (N - N / 2)*sizeof(int));
+        start = clock();
+        insert_sort(*unsorted, N);
+        end = clock();
+        insertSortTime += end - start;
+        start = clock();
+        insert_sort(*unsortedHalf1, N / 2);
+        insert_sort(*unsortedHalf2, N - N / 2);
+        end = clock();
+        mergeSortTime += end - start;
+        int *ptr1 = *unsortedHalf1, *ptr2 = *unsortedHalf2;
+        if (check == 1)//check result
+        {
+            for (size_t i = 0; i < N; ++i)
+            {
+                if ((*ptr1 < *ptr2) && (ptr1 < (*unsortedHalf1) + N / 2))
+                {
+                    assert(*ptr1 == (*unsorted)[i]);
+                    ++ptr1;
+                }
+                else
+                {
+                    assert(*ptr2 == (*unsorted)[i]);
+                    ++ptr2;
+                }
+            }
+        }
+        else
+        {
+            FILE *fp;
+            start = clock();
+            fp = fopen("insertSort.log", "w");
+            for (size_t i = 0; i < N; ++i)
+            {
+                fprintf(fp, "%d\n", (*unsorted)[i]);
+            }
+            fclose(fp);
+            end = clock();
+            insertSortTime += end - start;
+
+            start = clock();
+            fp = fopen("mergeSort.log", "w");
+            for (size_t i = 0; i < N; ++i)
+            {
+                if((*ptr1 < *ptr2) && (ptr1 < (*unsortedHalf1) + N / 2))
+                {
+                    fprintf(fp, "%d\n", *ptr1);
+                    ++ptr1;
+                }
+                else
+                {
+                    fprintf(fp, "%d\n", *ptr2);
+                    ++ptr2;
+                }
+            }
+            fclose(fp);
+            end = clock();
+            mergeSortTime += end - start;
+        }
+    }
+    fprintf(sortLog, "%d %f %f\n", N, (double)(insertSortTime * 1000) / (CLOCKS_PER_SEC*repeat), (double)(mergeSortTime * 1000) / (CLOCKS_PER_SEC*repeat));
+    fclose(sortLog);
+
+    return 0;
+}
+
+void insert_sort(int array[], size_t size)
+{
+    if (size <= 1)
+        return;
+    for (size_t i = 1; i < size; ++i)
+    {
+        for (size_t j = 0; j < i; ++j)
+        {
+            if (array[i] < array[j])
+            {
+                int tmp = array[i];
+                for (size_t k = i; k > j; --k)
+                    array[k] = array[k - 1];
+                array[j] = tmp;
+                break;
+            }
+        }
+    }
+}
