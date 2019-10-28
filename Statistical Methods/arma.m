43364ff7a69bae82e567ed85f54fff5f49728ac8
Zhijia Chen <zhijia.chen@temple.edu>
2019-10-28 18:56:09 -0400

    .

--- Statistical Methods/arma.m
+++ Statistical Methods/arma.m
@@ -1,21 +1,21 @@
-stockData = readtable('data_1900.csv');
+stockData = readtable('data_1950.csv');
 Date = datetime(arrayfun(@string, stockData.Date),'InputFormat','yyyy/MM/dd');
 SP500 = stockData.SP500;
 DividenDYield = stockData.DividendYield;
-k = 10;
+k = 12;
 Date = Date(k+1:end);
 SP500kYearsIncrease = (SP500(k+1:end)-SP500(1:end-k))./SP500(1:end-k);
 
 %EDA
 
-%figure;
-%plot(Date, SP500kYearsIncrease);
-maxLag = 15;
-%figure;
-%autocorr(SP500kYearsIncrease, 'NumLags',maxLag);
+figure;
+plot(Date, SP500kYearsIncrease);
+maxLag = 30;
+figure;
+autocorr(SP500kYearsIncrease, 'NumLags',maxLag);
 [acf,acfLags,acfBounds] = autocorr(SP500kYearsIncrease, 'NumLags',maxLag);
-%figure;
-%parcorr(SP500kYearsIncrease, 'NumLags',maxLag);
+figure;
+parcorr(SP500kYearsIncrease, 'NumLags',maxLag);
 [pacf,pacfLags,pacfBounds] = parcorr(SP500kYearsIncrease, 'NumLags',maxLag);
 
 
@@ -37,25 +37,29 @@ for mri = 1:length(MALags)
     MALagsChosen = MALags(1:mri);
     MALagsChosen = MALagsChosen(:)';
     for ari = 1:length(ARLags)
-        ARLagsChosen = ARLags(1:ari);
-        ARLagsChosen = ARLagsChosen(:)';
-        model = arima('ARLags',ARLagsChosen,'MALags',MALagsChosen,'Constant',0);
-        estModel = estimate(model,trainY);
-        [forecastY,YMSE] = forecast(estModel,testSize,trainY);
-        msqerr = sum((testY-forecastY).^2)/testSize;
-        if msqerr < minErr
-            minErr = msqerr;
-            bestModel = estModel;
-            bestForecast = forecastY;
-            bestARLags = ARLagsChosen;
-            bestMALags = MALagsChosen;
-        end   
+        try
+            ARLagsChosen = ARLags(1:ari);
+            ARLagsChosen = ARLagsChosen(:)';
+            model = arima('ARLags',ARLagsChosen,'MALags',MALagsChosen,'Constant',0);
+            estModel = estimate(model,trainY);
+            [forecastY,YMSE] = forecast(estModel,testSize,trainY);
+            msqerr = sum((testY-forecastY).^2)/testSize;
+            if msqerr < minErr
+                minErr = msqerr;
+                bestModel = estModel;
+                bestForecast = forecastY;
+                bestARLags = ARLagsChosen;
+                bestMALags = MALagsChosen;
+            end
+        catch
+            continue
+        end
     end
 end
 
 figure;
 hold on;
-trueDataHdl = plot(Date, SP500kYearsIncrease);
+trueDataHdl = plot(testDate, testY);
 forecastDataHdl = plot(testDate, bestForecast);
 legend([trueDataHdl forecastDataHdl],'truth','forecast', 'Location','NorthWest');
 hold off;
